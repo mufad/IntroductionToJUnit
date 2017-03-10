881ef7a83e5fc70f3ef26734153cc85b7721a17d
Monwar Jahan <mmufad@gmail.com>
2017-03-11 02:47:24 +0600

    Test File added

--- src/Introduction.java
+++ src/Introduction.java
@@ -1,7 +1,20 @@
+import com.sun.net.httpserver.Authenticator;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
 /**
  * Created by Mufad on 3/10/2017.
  */
 public class Introduction {
+    public static void main(String[] args) {
+        Result result = JUnitCore.runClasses(IntroductionTest.class);
 
+        for (Failure failure : result.getFailures()) {
+            System.out.println(failure.toString());
+        }
+
+        System.out.println(result.wasSuccessful());
+    }
 
 }
--- src/IntroductionTest.java
+++ src/IntroductionTest.java
@@ -1,5 +1,15 @@
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
 /**
  * Created by Mufad on 3/10/2017.
  */
 public class IntroductionTest {
+    @Test
+
+    public void testAdd() {
+        String str = "Junit is working fine";
+        assertEquals("Junit is working fine",str);
+    }
 }
