372882bf23bdb238968673fbef573949479387ca
Monwar Jahan <mmufad@gmail.com>
2017-05-23 11:48:24 +0600

    Update SearchEngine.java

--- src/SearchEngine.java
+++ src/SearchEngine.java
@@ -1,1 +1,29 @@
+/*
+This is the class that holds my methods for BFS
+*/
 
+public class SearchEngine{
+
+  List<Road_Items> dataList;
+  String source;
+  String destinition;
+  
+  public SearchEngine(List<Road_Items>dataList, String Source, String destinition){
+    
+    this.datalist=dataList;
+    this.source=source;
+    this.destinition=destinition;
+    
+  }
+  
+  public String getDestinition(){
+    
+    dataList.forEach((Road_Items road_item)->
+                    if(road_item.child.destinition.equals(destinition)){
+                      return destinition;
+                    }else{
+                    }
+                    );
+  }
+  
+}
